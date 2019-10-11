6fb639f82bf9b213f40b309b4e65a0cbeb15719a
surjashish.sengupta <surjashish.sengupta@cgi.com>
2019-10-11 15:21:19 +0530

    Penultimate stage

--- MovieApp Test/src/main/java/com/stackroute/MovieApp/MovieAppApplication.java
+++ MovieApp Test/src/main/java/com/stackroute/MovieApp/MovieAppApplication.java
@@ -1,9 +1,13 @@
 package com.stackroute.MovieApp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -17,3 +21,15 @@ public class MovieAppApplication {
 	}
 
 }
+
+@RefreshScope
+@RestController
+class MessageRestController {
+	@Value("${msg:Hello world - Config Server is not working..pelase check}")
+	private String msg;
+	@RequestMapping("/msg")
+	String getMsg() {
+		return this.msg;
+	}
+}
+
--- userservice/src/main/java/com/stackroute/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/stackroute/userservice/UserserviceApplication.java
@@ -1,8 +1,12 @@
 package com.stackroute.userservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +16,15 @@ public class UserserviceApplication {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+	@Value("${msg:Hello world - Config Server is not working..pelase check}")
+	private String msg;
+	@RequestMapping("/msg")
+	String getMsg() {
+		return this.msg;
+	}
 }
