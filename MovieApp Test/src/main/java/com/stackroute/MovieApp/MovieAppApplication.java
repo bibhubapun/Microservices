337da84a5788e252002ee8d2fe0b9a33f563c230
surjashish.sengupta <surjashish.sengupta@cgi.com>
2019-10-11 14:37:09 +0530

    Linking with eureka

--- MovieApp Test/src/main/java/com/stackroute/MovieApp/MovieAppApplication.java
+++ MovieApp Test/src/main/java/com/stackroute/MovieApp/MovieAppApplication.java
@@ -2,9 +2,11 @@ package com.stackroute.MovieApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieAppApplication {
 
 	public static void main(String[] args) {
